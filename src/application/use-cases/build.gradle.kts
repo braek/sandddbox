3d907ba4d018fa2bd8c411de4bd94e3cf4514e62
kristof.verbraeken <kristof.verbraeken@gmail.com>
2026-02-28 10:07:12 +0100

    Updated dependencies of use cases module

--- src/application/use-cases/build.gradle.kts
+++ src/application/use-cases/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+    implementation(project(":api"))
     implementation(project(":domain"))
     implementation(project(":vocabulary"))
     testImplementation(project(":test-doubles"))
