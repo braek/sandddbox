79728b6025ff8afd85f1096f0a8cb18e3a60b24a
kristof.verbraeken <kristof.verbraeken@gmail.com>
2026-02-28 01:03:51 +0100

    First implementation of DnsLabel class

---
+++ src/vocabulary/src/main/kotlin/be/koder/maxi/vocabulary/VocabularyExtensions.kt
@@ -0,0 +1,15 @@
+package be.koder.maxi.vocabulary
+
+fun String.sanitizeSingleLineString(): String {
+    return this.trim()
+        .replace("\n", "")
+        .replace("\t", "")
+        .replace("\r", "")
+}
+
+fun String.sanitizeMultiLineString(): String {
+    return this.trim()
+        .replace("\r", "")
+        .replace("\t", "")
+        .replace("\n{2,}".toRegex(), "\r\n\r\n")
+}
--- src/vocabulary/src/main/kotlin/be/koder/maxi/vocabulary/network/DnsLabel.kt
+++ src/vocabulary/src/main/kotlin/be/koder/maxi/vocabulary/network/DnsLabel.kt
@@ -1,3 +1,53 @@
 package be.koder.maxi.vocabulary.network
 
-class DnsLabel
+import be.koder.maxi.vocabulary.sanitizeSingleLineString
+
+class DnsLabel private constructor(str: String) {
+
+    private val value: String
+
+    init {
+        val sanitized = str.sanitizeSingleLineString().lowercase()
+        require(REGEXES.map { it.matches(sanitized) }.contains(true)) {
+            "Cannot create DNS label from string: '$str'"
+        }
+        this.value = sanitized
+    }
+
+    override fun toString(): String {
+        return value
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as DnsLabel
+        return value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
+    companion object Factory {
+        /**
+         * Accepted regular expressions for DNS labels
+         */
+        private val REGEXES = setOf(
+            /**
+             * Single wildcards are possible
+             */
+            "^\\*$".toRegex(),
+            /**
+             * Regular host label: minimum 1 character, maximum 63 characters and cannot start or end with a hyphen
+             */
+            "^(?!-)[a-z-0-9]{1,63}(?<!-)$".toRegex(),
+            /**
+             * Host label starting with an underscore
+             */
+            "^_(?!-)[a-z-0-9]{1,62}(?<!-)$".toRegex(),
+        )
+
+        fun create(str: String) = DnsLabel(str)
+    }
+}
