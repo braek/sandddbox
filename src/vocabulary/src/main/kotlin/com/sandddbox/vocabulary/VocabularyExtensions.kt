cb37ca4497fd780b57bb76255c862fe71aa6cc0e
kristof.verbraeken <kristof.verbraeken@gmail.com>
2026-03-01 15:55:55 +0100

    Stop replacing tabs in sanitization functions

--- src/vocabulary/src/main/kotlin/com/sandddbox/vocabulary/VocabularyExtensions.kt
+++ src/vocabulary/src/main/kotlin/com/sandddbox/vocabulary/VocabularyExtensions.kt
@@ -2,14 +2,12 @@ package com.sandddbox.vocabulary
 
 fun String.sanitizeSingleLineString(): String {
     return this.replace("\r", "")
-        .replace("\t", "")
         .replace("\n", "")
         .trim()
 }
 
 fun String.sanitizeMultiLineString(): String {
     return this.replace("\r", "")
-        .replace("\t", "")
         .replace("\n{2,}".toRegex(), "\n\n")
         .trim()
         .replace("\n".toRegex(), "\r\n")
