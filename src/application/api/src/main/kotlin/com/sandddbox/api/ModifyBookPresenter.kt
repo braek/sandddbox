2b98b618bea386e6257994befd78c0c5e5d7e805
kristof.verbraeken <kristof.verbraeken@gmail.com>
2026-03-24 11:08:46 +0100

    Updated the license

--- src/application/api/src/main/kotlin/com/sandddbox/api/ModifyBookPresenter.kt
+++ src/application/api/src/main/kotlin/com/sandddbox/api/ModifyBookPresenter.kt
@@ -3,7 +3,10 @@ package com.sandddbox.api
 import com.sandddbox.vocabulary.aggregate.BookId
 
 interface ModifyBookPresenter {
+
     fun bookDoesNotExist(bookId: BookId)
+
     fun noAuthorsProvided()
+
     fun modified(bookId: BookId)
 }
