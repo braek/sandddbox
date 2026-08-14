70faf19190eb37da17bd77844133ebcb81cd5cc0
Kristof Verbraeken <kristof.verbraeken@gmail.com>
2026-08-14 22:30:11 +0200

    Downgraded AssertJ to trigger Renovate

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
          */
         create("testLibs") {
             version("junit", "6.0.3")
-            version("assertj", "3.27.7")
+            version("assertj", "3.27.6")
             library("assertj", "org.assertj", "assertj-core").versionRef("assertj")
         }
     }
